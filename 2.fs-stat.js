b33791206a5aca73ea078b9d9859e35af0ff2845
Rubendvb <rubenvasquez556@gmail.com>
2023-07-27 04:12:17 -0300

    adicionando scripts promises

---
+++ 2.fs-stat.js
@@ -0,0 +1,10 @@
+const fs = require("node:fs");
+
+const stats = fs.statSync("./arquivo.txt");
+
+console.log(
+  stats.isFile(), // Verifica se é um arquivo
+  stats.isDirectory(), // Verifica se é um diretório
+  stats.isSymbolicLink(), // Verifica se é um link simbólico
+  stats.size // Tamanho do arquivo em bytes
+);
---
+++ 3.fs-readFile.js
@@ -0,0 +1,15 @@
+const fs = require("node:fs");
+
+console.log("Lendo o primeiro arquivo");
+
+fs.readFile("./arquivo.txt", "utf-8", (err, text) => {
+  console.log("primeiro texto", text);
+});
+
+console.log("Fazendo coisas enquanto se lê o primeiro arquivo");
+
+console.log("Lendo o segundo arquivo");
+
+fs.readFile("./arquivo2.txt", "utf-8", (err, text) => {
+  console.log("segundo texto", text);
+});
