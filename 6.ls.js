a4f1d34b99c8f28850b1daebdb3ba14b6e013548
Rubendvb <rubenvasquez556@gmail.com>
2023-07-29 02:39:48 -0300

    adicionando scripts 5,6,7,8

---
+++ 6.ls.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs");
+
+// Lê o conteúdo do diretório atual
+fs.readdir(".", (err, files) => {
+  if (err) {
+    console.error("Erro ao ler o diretório: ", err);
+    return;
+  }
+
+  // Itera sobre os arquivos encontrados e imprime seus nomes
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
---
+++ 7.process.js
@@ -0,0 +1,16 @@
+// // Argumentos de entrada
+// console.log(process.argv);
+
+// // Controla o processo e sua saída
+// process.exit();
+
+// // Podemos controlar eventos do processo
+// process.on("exit", () => {
+//   // limpar recursos
+// });
+
+// // current working directory
+// console.log(process.cwd());
+
+// // platform
+// console.log(process.env.PEPITO);
