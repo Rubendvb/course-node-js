1b96b6f5c9d8ef022b1bbbd546fc690b570f992d
Rubendvb <rubenvasquez556@gmail.com>
2023-07-30 01:26:12 -0300

    criando API

---
+++ aula-2/1.http.js
@@ -0,0 +1,35 @@
+const http = require('node:http')
+const fs = require('node:fs')
+
+const desiredPort = process.env.PORT ?? 3000
+
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.end('<h1>Bem-vindo a minha página de inicio</h1>')
+  } else if (req.url === '/image') {
+    fs.readFile('./image-qa.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contato') {
+    res.statusCode = 200
+    res.end('<h1>Contato</h1>')
+  } else {
+    res.statusCode = 400 // Not found
+    res.end('<h1>404</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
